1cc63a955b03f02f478180e45da923d2a1361daf
HP <HP@LAPTOP-1PCDHDIA>
2023-03-18 21:56:14 +0700

    Add project files.

---
+++ NguyenHungCuong2011060110/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NguyenHungCuong2011060110.MvcApplication" Language="C#" %>
---
+++ NguyenHungCuong2011060110/Migrations/202303150633388_PopulateCategoryTable.cs
@@ -0,0 +1,20 @@
+﻿namespace NguyenHungCuong2011060110.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO CATEGORIES(ID,NAME) VALUES (1,'IT')");
+            Sql("INSERT INTO CATEGORIES(ID,NAME) VALUES (2,'Marketing')");
+            Sql("INSERT INTO CATEGORIES(ID,NAME) VALUES (3,'Oto')");
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}
